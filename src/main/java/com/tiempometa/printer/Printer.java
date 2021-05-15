63e628ad7d8fd27e981b26e63be40260ec9b8845
Gerardo Tasistro <gtasistro@tiempometa.com>
2021-05-15 05:09:12 -0500

    Set myself as copyright owner

--- src/main/java/com/tiempometa/printer/Printer.java
+++ src/main/java/com/tiempometa/printer/Printer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 TiempoMeta
+ * Copyright (c) 2019 Gerardo Esteban Tasistro Giubetic
  * 
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
